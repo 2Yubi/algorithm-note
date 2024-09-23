b6f00de870f102c2aa8a43236c0222d234a49d16
2Yubi <hamtol98@gmail.com>
2024-09-23 20:35:57 +0900

    Add:baekjoon-2468_dfs

---
+++ code/2.graph-theory/2468_2.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[101][101], visited[101][101], n, ret = 1;
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
+
+void dfs(int y, int x, int d){
+    visited[y][x] = 1;
+
+    for(int i = 0; i < 4; i++){
+        int ny = dy[i] + y;
+        int nx = dx[i] + x;
+
+        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+
+        if(!visited[ny][nx] && a[ny][nx] > d) dfs(ny, nx, d);
+    }
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for(int d = 1; d < 101; d++){
+        fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(a[i][j] > d && !visited[i][j]){
+                    dfs(i, j, d); cnt++;
+                }
+            }
+        }
+        ret = max(ret, cnt);
+    }
+    cout << ret << "\n";
+
+    return 0;
+}
---
+++ code/2.graph-theory/2583.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int m, n, k;
+
+int main(){
+    cin >> m >> n >> k;
+
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            
+        }
+    }
+    return 0;
+}
+
+
+/*
+0 2 - 4 4 -> 
+1 1 - 2 5
+4 0 - 6 2
+
+
+(0, 2) - (4, 4)
+(1, 4) (1, 3) 
+(2, 4) (2, 3)
+(3, 4) (3, 3)
+(4, 4) (4, 3)
+
+
+*/
