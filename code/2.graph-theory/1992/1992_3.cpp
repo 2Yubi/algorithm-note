6fd3fe2583dcb9492ae78ad187e970faa6702e22
2Yubi <hamtol98@gmail.com>
2024-10-08 22:01:39 +0900

    baekjoon-2910:map

--- code/2.graph-theory/1992/1992_3.cpp
+++ code/2.graph-theory/1992/1992_3.cpp
@@ -1,17 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n;
-char a[65][65];
-string s;
-
-
+typedef long long ll;
+int n, mx;
+ll c;
+ll cnt[1004], a[1004];
 
 int main(){
-    cin >> n;
+    cin >> n >> c;
     for(int i = 0; i < n; i++){
-        cin >> s;
-        for(int j = 0; j < n; j++){
-            a[i][j] = s[j];
+        cin >> c;
+        cnt[c]++;
+    }
+
+    for(int i = 0; i < c; i++){
+        for(int j = 0; j < a[i]; j++){
+            cout << i << " ";
         }
     }
 
