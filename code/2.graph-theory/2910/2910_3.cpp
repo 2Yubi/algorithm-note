d8743436a374b8290713346e7b7544c94db609e0
2Yubi <hamtol98@gmail.com>
2024-10-10 23:43:16 +0900

    baekjoon-2910_3

---
+++ code/2.graph-theory/2910/2910_3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll INF = 1e18;
+int n, c, a[1004];
+vector<pair<int, int>> v;
+map<int, int> mp, mp_first;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> c;
+
+
+
+    return 0;
+}
