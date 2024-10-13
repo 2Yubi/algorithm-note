3acdbaf9d609018208f6527d59ea75e517b50bc1
*hwang <hyeonjong.hwang@iijglobal.co.jp>
2024-10-13 17:13:38 +0900

    baekjoon-2910:map,set

--- code/2.graph-theory/2910/2910_3.cpp
+++ code/2.graph-theory/2910/2910_3.cpp
@@ -1,18 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-const ll INF = 1e18;
 int n, c, a[1004];
-vector<pair<int, int>> v;
 map<int, int> mp, mp_first;
+vector<pair<int, int>> v;
 
+//bool cmp(pair<int,int> a, pair<int,int> b){
+//	if(a.first == b.first){
+//		return mp_first[a.second] < mp_first[b.second];
+//	}
+//	return a.first > b.first;
+//}
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> n >> c;
-
-
-
-    return 0;
+	cin >> n >> c;
+	for(int i = 0; i < n; i++){
+		cin >> a[i]; mp[a[i]]++;
+		if(mp_first[a[i]] == 0) mp_first[a[i]] = i; 
+	}
+	
+	for(auto it : mp_first) cout << it.first << " " << it.second << "\n";
+	
+//	for(auto i : mp) v.push_back({i.second, i.first});
+//
+//	for(auto i : v){
+//		for(int j = 0; j < i.first; j++){
+//			cout << i.second << " ";
+//		}
+//	}
+	
+	
+	return 0;
 }
+
+
+
+
+/* 
+
+
+
+*/
