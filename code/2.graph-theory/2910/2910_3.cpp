e65fc2c72cdb5179de365adaeb20be9677c4f450
2Yubi <hamtol98@gmail.com>
2024-10-14 15:59:04 +0900

    baekjoon2910_4:map, sort

--- code/2.graph-theory/2910/2910_3.cpp
+++ code/2.graph-theory/2910/2910_3.cpp
@@ -1,42 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+map<int,int> mp, mp_first;
+vector<pair<int,int>> v;
 int n, c, a[1004];
-map<int, int> mp, mp_first;
-vector<pair<int, int>> v;
-
-//bool cmp(pair<int,int> a, pair<int,int> b){
-//	if(a.first == b.first){
-//		return mp_first[a.second] < mp_first[b.second];
-//	}
-//	return a.first > b.first;
-//}
+bool cmp(pair<int,int> a, pair<int,int> b){
+	if(a.first == b.first){
+		return mp_first[a.second] < mp_first[b.second];
+	}
+	return a.first > b.first;
+}
 
 int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> c;
 	for(int i = 0; i < n; i++){
 		cin >> a[i]; mp[a[i]]++;
-		if(mp_first[a[i]] == 0) mp_first[a[i]] = i; 
+		if(mp_first[a[i]] == 0) mp_first[a[i]] = i + 1;
 	}
-	
-	for(auto it : mp_first) cout << it.first << " " << it.second << "\n";
-	
-//	for(auto i : mp) v.push_back({i.second, i.first});
-//
-//	for(auto i : v){
-//		for(int j = 0; j < i.first; j++){
-//			cout << i.second << " ";
-//		}
-//	}
-	
-	
+
+	for(auto it : mp) v.push_back({it.second, it.first});
+
+	sort(v.begin(), v.end(), cmp);
+
+	for(auto i : v){
+		for(int j = 0; j < i.first; j++){
+			cout << i.second << " ";
+		}
+	} 
+
 	return 0;
 }
-
-
-
-
-/* 
-
-
-
-*/
