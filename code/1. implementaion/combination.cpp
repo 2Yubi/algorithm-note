d03c8fc7213a7ce61871c14e9e875431dfc85b19
2Yubi <hamtol98@gmail.com>
2024-10-21 23:56:12 +0900

    study:combination-for

--- code/1. implementaion/combination.cpp
+++ code/1. implementaion/combination.cpp
@@ -14,6 +14,7 @@ void combi(int start, vector<int> b){
     }
     for(int i = start + 1; i < n; i++){
         b.push_back(i);
+        // 0, 1, 2, 3, 4
         combi(i, b);
         b.pop_back();
     }
@@ -27,5 +28,17 @@ int main(){
 
 
 // 2. forで実装
-
+int n = 5;
+int k = 3;
+int a[5] = {1, 2, 3, 4, 5};
+int main(){
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            for(int k = j + 1; k < n; k++){
+                cout << i << " " << j << " " << k << "\n"; 
+            }
+        }
+    }
+    return 0;
+}
 
