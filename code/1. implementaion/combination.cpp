9582761e53161ec2b7d11bede7ff53d256386ce3
2Yubi <hamtol98@gmail.com>
2024-10-29 23:53:33 +0900

    recursive:combination

--- code/1. implementaion/combination.cpp
+++ code/1. implementaion/combination.cpp
@@ -4,10 +4,10 @@ int n = 5, k = 3, a[5] = {1, 2, 3, 4, 5};
 
 // 1. 再帰で実装
 void print(vector<int> b){
-    for(int i : b){
-        cout << i << " ";
-        cout << "\n";
+    for(int it : b){
+        cout << it << " ";
     }
+    cout << "\n";
 }
 
 void combi(int start, vector<int> b){
@@ -15,19 +15,88 @@ void combi(int start, vector<int> b){
         print(b);
         return;
     }
+
     for(int i = start + 1; i < n; i++){
         b.push_back(i);
         combi(i, b);
         b.pop_back();
     }
-    return;
 }
+
 
 int main(){
     vector<int> b;
     combi(-1, b);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void print(vector<int> b){
+//     for(int it : b){
+//         cout << it << " ";
+//     }
+//     cout << "\n";
+// }
+// void combi(int start, vector<int> b){
+//     if(b.size() == k){
+//         print(b);
+//         return;
+//     }
+//     for(int i = start + 1; i < n; i++){
+//         b.push_back(i);
+//         combi(i, b);
+//         b.pop_back();
+//     }
+
+// }
+// int main(){
+//     vector<int> b;
+//     combi(-1, b);
+//     return 0;
+// }
+
+
+// 2. forで実装
+
+
+// 1. 再帰で実装
+// void print(vector<int> b){
+//     for(int i : b){
+//         cout << i << " ";
+//         cout << "\n";
+//     }
+// }
+
+// void combi(int start, vector<int> b){
+//     if(b.size() == k){
+//         print(b);
+//         return;
+//     }
+//     for(int i = start + 1; i < n; i++){
+//         b.push_back(i);
+//         combi(i, b);
+//         b.pop_back();
+//     }
+//     return;
+// }
+
+// int main(){
+//     vector<int> b;
+//     combi(-1, b);
+//     return 0;
+// }
 
 
 // 2. forで実装
@@ -122,4 +191,4 @@ int main(){
 //         }
 //     }
 //     return 0;
-// }
+// }f
